0c609f84d17b9d8440fa9198111dfc7162bf3cf9
Navjeet Ahalawat <ahlawat.0211@gmail.com>
2022-01-12 10:19:38 +0530

    updated file

--- run_pair_distances.sh
+++ run_pair_distances.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-msmb ContactFeaturizer --transformed TRANSFORMED_HEAVY --top ../ref.pdb --trjs ../DATA_*/RUN*/frame0.xtc --contacts contacts_pairs.txt
+msmb ContactFeaturizer --transformed TRANSFORMED_HEAVY --top ../ref.pdb --trjs ../DATA_*/RUN*/frame0.xtc --contacts contact_pairs.txt
 
